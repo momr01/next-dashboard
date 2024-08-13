30194813158c5b3245f5d17c094813f48b94e3af
Maximiliano Montaña <maxi.omr01@gmail.com>
2024-08-13 18:15:30 -0300

    add footer, pagination, products, search and users components and UI

---
+++ src/app/dashboard/products/add/page.tsx
@@ -0,0 +1,39 @@
+//import { addProduct } from "@/app/lib/actions";
+import styles from "@/app/ui/dashboard/products/addProduct/addProduct.module.css";
+
+const AddProductPage = () => {
+  return (
+    <div className={styles.container}>
+      <form
+        action={
+          //addProduct
+          ""
+        }
+        className={styles.form}
+      >
+        <input type="text" placeholder="Title" name="title" required />
+        <select name="cat" id="cat">
+          <option value="general">Choose a Category</option>
+          <option value="kitchen">Kitchen</option>
+          <option value="phone">Phone</option>
+          <option value="computer">Computer</option>
+        </select>
+        <input type="number" placeholder="Price" name="price" required />
+        <input type="number" placeholder="Stock" name="stock" required min={1} />
+        <input type="text" placeholder="Color" name="color" />
+        <input type="text" placeholder="Size" name="size" />
+        <textarea
+          required
+          name="desc"
+          id="desc"
+          // rows="16"
+          rows={16}
+          placeholder="Description"
+        ></textarea>
+        <button type="submit">Submit</button>
+      </form>
+    </div>
+  );
+};
+
+export default AddProductPage;
